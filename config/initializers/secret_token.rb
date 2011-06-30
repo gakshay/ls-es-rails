3c25d997a2673c40045710ece71c676ff1e974b6
Akshay Gupta <akshaygupta.dev@gmail.com>
2011-06-30 18:48:47 +0530

    logstash with ROR as frontend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsRails::Application.config.secret_token = '210d4d6e12253ad03ba43e637a25d5cbaf43fc94a6f32784c848b9f738f6f81d53cd927d5ed90ece64dd3ecd4ebe7faa4869a9bd05830c89dff1cf1e7c769948'
